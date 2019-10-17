50556ae71e6efe77fdc7863c27f3ecb31edde8e5
melkam beyene <melkam3392beyu2gmail.com>
2019-10-17 15:41:37 -0400

    authentication commit

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -25,52 +25,30 @@ module.exports.register=(req,res)=>{
     role:req.body.role
 
   });
-  if(req.body.role=="professional"){
-    user.findOne({email:req.body.email},(err,user)=>{
-      // if(user) return res.json({succes:false,message:"Email address alredy exist"}); 
+  
+    user.findOne({username:req.body.username},(err,result)=>{
+      if(user) return res.json({succes:false,result:"User alredy exits"}); 
       bcrypt.genSalt(10,(err,salt)=>{
+        if(err) return res.json({succes:false,result:err})
         bcrypt.hash(newUser.password,salt,(err,hash)=>{
             if(err)return res.json({succes:false,message:err});
             newUser.password=hash;
             newUser.save((err)=>{
-              if(err) return res.json({succes:false,message:err});
-              var token = new Token({ _userId: user.id, token: crypto.randomBytes(16).toString('hex') });
-              token.save((err)=>{
-                if(err) res.json({succes:false,message:err});
-                var transporter=nodemailer.createTransport({service:'gmail',host:'smtp.gmail.com',auth:{user:'melkam3392beyu@gmail.com',pass:'igivemylifetojesus0912'}});
-            var mailOptions = { from: 'melkam3392beyu@gmail.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
-              transporter.sendMail(mailOptions,(err)=>{
-                if(err) return res.json({succes:true,message:err});
-                return res.json({success:true,message:'A verification email has been sent to ' + user.email + '.'})
-              })
-              })
+              if(err) return res.json({succes:false,result:err});
+              return res.json({succes:true,result:result})
             
             })
         });
     });
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
     })                                                                                        
-  }
-  
+
   
 };
 
 
 
-module.exports.emailConfirmation=(req,res)=>{
- Token.findOne({token:req.body.token},(err,token)=>{
-  if (!token) return res.status(400).send({ type: 'not-verified', msg: 'We were unable to find a valid token. Your token my have expired.' });
-  user.findOne({_id:_userId,email:req.body.email},(err,user)=>{
-    if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
-      if (user.isVerified) return res.status(400).send({ type: 'already-verified', msg: 'This user has already been verified.' });
-    user.isVerified=true;
-    user.save((err)=>{
-      if(err)return res.json({succes:false,message:err.message});
-      res.json({succes:true,message:"the accout is verified you can login"});
-    })
-    })
- })
-}
+
 
 
 module.exports.login=(req,res)=>{
@@ -96,10 +74,16 @@ module.exports.login=(req,res)=>{
         }
 
         );
-        return res.json({
-          success:true,
-          token:token
-        });
+        return res.json({success:true,token:token})
+      //   if(req.body.role=="professional"){
+      //     return res.json({
+      //       success:true,
+      //       token:token,
+      //       message:"Professional loged in"
+      //     });
+      //   }else{
+      //     return res.json({succes:false,token:token,message:"Student loged in"})
+      //   }
 
       }else{
         return res.json({success:false,message:"Wrong password"})
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,28 +1,16 @@
 const  express=require('express');
 const router=express.Router();
-const UserController=require('../controllers/users')
+const UserController=require('../controllers/users');
+const checkAuth=require('../config/checkAuth');
 
 
 
 router.post('/register',UserController.register);
 router.post('/login',UserController.login);
-router.get('/profile',verifyToken,UserController.profile);
+router.get('/profile',checkAuth,UserController.profile);
 
 // router.delete();
 
-function verifyToken(req,res,next){
-  const bearerHeader=req.headers['authorization'];
-  if(typeof bearerHeader!=='undefined'){
-// sprlit at thes space
-const bearer=bearerHeader.split(' ');
-//get tokent from array 
-const bearerToken=bearer[1];
-req.token=bearerToken;
 
-next();
-  }else{
-    res.sendStatus(403);
-  }
-}
 
 module.exports=router
