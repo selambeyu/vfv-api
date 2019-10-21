ece1f0329c9ce5f0e43e1ab4d2bde6e869515436
melkam beyene <melkam3392beyu2gmail.com>
2019-10-21 06:07:17 -0400

    update on admin

---
+++ server/controllers/admin.js
@@ -0,0 +1,94 @@
+// const User =require('../services/users');
+const bcrypt=require('bcryptjs')
+const express = require('express');
+const router = express.Router();
+const passport =require('passport');
+const jwt=require('jsonwebtoken');
+const nodemailer=require('nodemailer')
+router.use(passport.initialize());
+router.use(passport.session());
+const admin=require('../models/users');
+//const db=require('../config/db_connection');
+const config=require('../config/config');
+const Token=require('../models/token')
+
+/*  */
+//require('../config/passport')(passport);
+
+module.exports.register=(req,res)=>{
+  let newAdmin=new admin({
+    firstname:req.body.firstname,
+    lastname:req.body.lastname,
+    username:req.body.username,
+    email:req.body.email,
+    password:req.body.password
+   
+
+  });
+  
+    admin.findOne({username:req.body.username},(err,result)=>{
+      if(admin) return res.json({succes:false,result:"admin alredy exits"}); 
+      bcrypt.genSalt(10,(err,salt)=>{
+        if(err) return res.json({succes:false,result:err})
+        bcrypt.hash(newAdmin.password,salt,(err,hash)=>{
+            if(err)return res.json({succes:false,message:err});
+            newAdmin.password=hash;
+            newAdmin.save((err)=>{
+              if(err) return res.json({succes:false,result:err});
+              return res.json({succes:true,result:result})
+            
+            })
+        });
+    });
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    })                                                                                        
+
+  
+};
+
+
+
+
+
+
+module.exports.login=(req,res)=>{
+  admin.findOne({username:req.body.username},(err,user)=>{
+    if(err)throw err;
+    bcrypt.compare(req.body.password,user.password,(err,isMatch)=>{
+      if(err)throw err;
+      if(isMatch){
+        const token=jwt.sign({user:user},config.secret,{expiresIn:604880})
+       
+        return res.json({success:true,token:token})
+      
+
+      }else{
+        return res.json({success:false,message:"Wrong password"})
+      }
+
+    })
+  })
+}
+
+
+// /get authenticated user profile
+
+module.exports.profile=(req,res)=>{
+  // console.log(req.user);
+  jwt.verify(req.token,config.secret,(err,authData)=>{
+      if(err)res.json({success:false,message:err});
+      res.json({
+        succes:true,
+        authData
+      })
+
+  })
+  
+};
+
+
+
+
+
+
+
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,100 +7,16 @@ router.use(passport.session());
 const Admin=require('../models/admin');
 //const db=require('../config/db_connection');
 const config=require('../config/config');
+const AdminController=require('../controllers/admin')
 
 /*  */
 // require('../config/passport')(passport);
 
-router.post('/register',(req,res)=>{
-  let newAdmin=new Admin({
-    firstname:req.body.firstname,
-    lastname:req.body.lastname,
-    username:req.body.username,
-    email:req.body.email,
-    password:req.body.password
 
-  });
-  Admin.addAdmin(newAdmin,(err,user)=>{
-    if(err){
-      let message='';
-      if(err.errors.username)messsage="Username is alredy taken  ";
-      if(err.errors.email)message +="Email alredy exits";
-      return res.json({
-        success:false,
-        message
-      });
-    } else {
-      return res.json({
-        success:true,
-        message:"Admin is registered succesfully"
-      });
-    }
-  });
-});
+router.post('/register',AdminController.register);
+router.post('/login',AdminController.login);
+router.get('/profile',verifyToken,AdminController.profile);
 
-
-
-router.post('/login',(req,res)=>{
-  const username=req.body.username;
-  const password=req.body.password;
-  Admin.getAdminByUsername(username,(err,user)=>{
-    if(err)throw err;
-    if(!user){
-      return res.json({
-        success:false,
-
-      });
-    }
-    Admin.comparePassword(password,user.password,(err,isMatch)=>{
-      if(err)throw err;
-      if(isMatch){
-        const token=jwt.sign({
-        role:"Admin",
-          data:{
-            _id:user._id,
-            firstname:user.firstname,
-            username:user.username,
-            lastname:user.lastname,
-            email:user.email,
-            
-
-          }
-        },config.secret,{
-          expiresIn:604880
-        }
-
-        );
-        return res.json({
-          success:true,
-          token:token
-        });
-      }else{
-        return res.json({
-          success:true,
-          message:"Wrong password"
-        })
-      }
-    });
-  });
-});
-
-
-// /get authenticated user profile
-
-router.get('/profile',verifyToken,(req,res)=>{
-  console.log(req.user);
-  jwt.verify(req.token,config.secret,(err,authData)=>{
-    if(err){
-      res.sendStatus(403);
-    }else{
-      res.json({
-        message:"user profile",
-        authData
-      })
-    }
-  })
-  
-});
 
 
 function verifyToken(req,res,next){
