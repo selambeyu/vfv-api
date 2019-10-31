a25a9c596c8863786a5bc1caba476c3299f52a26
melkam beyene <melkam3392beyu2gmail.com>
2019-10-31 07:29:01 -0400

    update

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use('/api/user/professional',profesionalRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // next(createError(404));
 });
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
